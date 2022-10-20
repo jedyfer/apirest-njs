c8c9364519ec92f770cb55587cd19464c63941d1
Jediael Fernández <jedyfer21@gmail.com>
2022-10-19 20:52:03 -0500

    Node.js Mysql Api Rest (endpoints)

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world")
+import express from 'express'
+
+const app = express()
+
+app.get('/employees', (req, res) => res.send('Obteniendo empleados'))
+
+app.post('/employees', (req, res) => res.send('Creando empleados'))
+
+app.put('/employees', (req, res) => res.send('Actualizando empleados'))
+
+app.delete('/employees', (req, res) => res.send('Eliminando empleados'))
+
+app.listen(3000)
+
+console.log('Server running on port 3000')
+
+/* 20:23 video */
