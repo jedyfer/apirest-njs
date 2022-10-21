6838dd2a451523ef331259d22937c98445e2a076
Jediael Fernández <jedyfer21@gmail.com>
2022-10-20 21:52:57 -0500

    Node.js Mysql Api Rest (rutas del servidor)

--- server/index.js
+++ server/index.js
@@ -1,21 +1,14 @@
-import express from 'express'
-import { pool } from './database.js'    //  cuando uso modulos propios necesario el .js cuando no no es necesario
+import express from 'express'   //  cuando uso modulos propios necesario el .js cuando no no es necesario
+import employeesRoutes from './routes/employes.routes.js'
+import indexRoutes from './routes/index.routes.js'
 
 const app = express()
 
 //  usando la conexion de db
-app.get('/ping', async (req, res) => {
-    const [result] = await pool.query('SELECT 1 + 1 AS result')
-    res.json(result[0])
-})
+app.use(indexRoutes)
 
-app.get('/employees', (req, res) => res.send('Obteniendo empleados'))
-
-app.post('/employees', (req, res) => res.send('Creando empleados'))
-
-app.put('/employees', (req, res) => res.send('Actualizando empleados'))
-
-app.delete('/employees', (req, res) => res.send('Eliminando empleados'))
+//  usando las rutas
+app.use(employeesRoutes)
 
 app.listen(3000)
 
